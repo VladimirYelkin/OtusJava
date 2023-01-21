9c21783ec77af7256d90b27de24dabed36759578
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-01-21 13:06:36 +0400

    the first implementation of CustomerService class

--- hw02-generics/src/main/java/homework/CustomerService.java
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -1,23 +1,50 @@
 package homework;
 
 
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class CustomerService {
 
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    TreeMap<Customer, String> customers = new TreeMap<>((customer1, customer2) -> {
+        if (customer1.getScores() <= customer2.getScores()) {
+            if (customer1.getScores() < customer2.getScores()) {
+                return -1;
+            } else return 0;
+        } else {
+            return 1;
+        }
+    }
+    );
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        var result = customers.firstEntry();
+        Map.Entry<Customer, String> res1;
+        if (result != null) {
+            res1 = new AbstractMap.SimpleEntry<Customer, String>(
+                    new Customer(result.getKey().getId(), result.getKey().getName(), result.getKey().getScores()), result.getValue());
+        } else {
+            res1 = null;
+        }
+        return res1;
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        var result = customers.higherEntry(customer);
+        Map.Entry<Customer, String> res1;
+        if (result != null) {
+            res1 = new AbstractMap.SimpleEntry<Customer, String>(
+                    new Customer(result.getKey().getId(), result.getKey().getName(), result.getKey().getScores()), result.getValue());
+        } else {
+            res1 = null;
+        }
+        return res1;
     }
 
     public void add(Customer customer, String data) {
-
+        customers.put(customer, data);
     }
 }

93be11f464ad6b0d5bf47c3de77817312f98dcc7
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-01-21 19:22:42 +0400

    CustomerReverseOrder reformat code

--- hw02-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw02-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,18 +1,19 @@
 package homework;
 
 
-import java.util.*;
+import java.util.ArrayDeque;
 
 public class CustomerReverseOrder {
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-     private final ArrayDeque<Customer> customers = new ArrayDeque<>();
+    private final ArrayDeque<Customer> customers = new ArrayDeque<>();
+
     public void add(Customer customer) {
         customers.push(customer);
     }
 
     public Customer take() {
-        return customers.pop(); // это "заглушка, чтобы скомилировать"
+        return customers.pop();
     }
 }

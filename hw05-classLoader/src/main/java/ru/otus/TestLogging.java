6748d8641fb64989f62790aa93c48b12e6e2b136
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-02-15 13:36:30 +0400

    HomeWork5 Test class Logging (TestLogging)

---
+++ hw05-classLoader/src/main/java/ru/otus/TestLogging.java
@@ -0,0 +1,25 @@
+package ru.otus;
+
+import ru.otus.annotations.Log;
+
+import java.io.Serializable;
+
+public class TestLogging implements TestLoggingInterface, Serializable {
+
+    @Log
+    public void calculation(int param) {
+        System.out.println("calculation origin, param = 6");
+    }
+
+    @Override
+
+    public void calculation(int param1, int param2) {
+        System.out.println("calculation origin, param1 = " + param1 + " param2 = " + param2);
+    }
+
+    @Override
+    @Log
+    public void calculation(int param1, int param2, String paramString) {
+        System.out.println("calculation origin, param1 = " + param1 + " param2 = " + param2 + " param3 = " + paramString);
+    }
+}

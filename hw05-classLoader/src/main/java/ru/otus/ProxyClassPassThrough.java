45ef1bb39089486a571f133ba007103d6ee671f2
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-02-16 14:57:26 +0400

    HomeWork5 add ProxyClassPassThrough // create proxy object by call constructor of this class whith parametrs: object of original class, interface with methods to logging;

---
+++ hw05-classLoader/src/main/java/ru/otus/ProxyClassPassThrough.java
@@ -0,0 +1,61 @@
+package ru.otus;
+
+import ru.otus.annotations.Log;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProxyClassPassThrough {
+    @SuppressWarnings("unchecked")
+    public static <T> T proxingMyClass(T myClass, Class iface) {
+        InvocationHandler handler = new ObjectInvocationHandler(myClass, iface);
+        return (T) Proxy.newProxyInstance(
+                iface.getClassLoader(),
+                new Class<?>[]{iface},
+                handler);
+    }
+
+    static class ObjectInvocationHandler implements InvocationHandler {
+
+        private final Object myClass;
+
+        private final List<Method> loggingMethods;
+
+        public ObjectInvocationHandler(Object myClass, Class iface) {
+            this.myClass = myClass;
+            List<Method> loggingMethodsOfClassByAnnotation = Arrays.stream(myClass.getClass().getDeclaredMethods())
+                    .filter(m -> m.isAnnotationPresent(Log.class))
+                    .collect(Collectors.toList());
+            List<Method> methodsOfLoggingInterface = Arrays.stream(iface.getDeclaredMethods()).collect(Collectors.toList());
+
+            loggingMethods = getLoggingMethodsOfInterfaceByAnnotationInClass(methodsOfLoggingInterface, loggingMethodsOfClassByAnnotation);
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (loggingMethods.contains(method)) {
+                System.out.print("executed method:" + method.getName());
+                System.out.print(Arrays.stream(args).map(Object::toString).collect(Collectors.joining(",", "[", "]\n")));
+            }
+            return method.invoke(myClass, args);
+        }
+
+        private List<Method> getLoggingMethodsOfInterfaceByAnnotationInClass(List<Method> listMethodsOfInterface, List<Method> listMethodsOfClass) {
+            return listMethodsOfInterface.stream()
+                    .filter(methodByInterface -> isMethodOfInterfaceInClassMethodsWithAnnotation(methodByInterface, listMethodsOfClass))
+                    .collect(Collectors.toList());
+        }
+
+        private boolean isMethodOfInterfaceInClassMethodsWithAnnotation(Method methodByInterface, List<Method> listMethodsOfClass) {
+            return listMethodsOfClass.stream()
+                    .anyMatch(methodByClass ->
+                            methodByInterface.getName().equals(methodByClass.getName())
+                                    && Arrays.deepEquals(methodByInterface.getParameterTypes(), methodByClass.getParameterTypes()));
+        }
+    }
+}
+

b09197fac42e1ad0f5490615a78fd946e509c8f4
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-03-07 10:41:22 +0400

    HomeWork6 BanknotesImpl moved to package

--- hw06-solid/src/main/java/ru/otus/BanknotesImpl.java
+++ hw06-solid/src/main/java/ru/otus/BanknotesImpl.java
@@ -1,9 +1,13 @@
+package ru.otus;
+
 public enum BanknotesImpl implements Banknotes {
     FIFTY(50),
     ONEHUNDRED(100),
     TWOHUNDRED(200),
     FIVEHUNDRED(500),
-    ONETHOUSAND(1000);
+    ONETHOUSAND(1000),
+    TWOTHOUSAND(2000),
+    FIVETHOUSAND(5000);
     private final int value;
 
     BanknotesImpl(int value) {
@@ -13,5 +17,10 @@ public enum BanknotesImpl implements Banknotes {
     @Override
     public int getNominal() {
         return value;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "(" + value + ")";
     }
 }

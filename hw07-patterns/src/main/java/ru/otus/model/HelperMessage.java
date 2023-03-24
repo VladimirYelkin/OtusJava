917d55a29048751b5317a822a29dafe05a2e0514
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-03-24 12:05:33 +0400

    HomeWork7 change name method in HelperMessage

--- hw07-patterns/src/main/java/ru/otus/model/HelperMessage.java
+++ hw07-patterns/src/main/java/ru/otus/model/HelperMessage.java
@@ -7,7 +7,7 @@ public class HelperMessage {
     private HelperMessage() {
     }
 
-    public static Message deepCopy (Message message) {
+    public static Message getDeepCopy(Message message) {
         ObjectForMessage objectForMessage = new ObjectForMessage();
         objectForMessage.setData(new ArrayList<>(message.getField13().getData()));
         return message.toBuilder().field13(objectForMessage).build();

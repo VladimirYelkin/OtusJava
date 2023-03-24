c8882cff67c7f3e72af96babe88e9a45072c131b
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-03-24 12:04:56 +0400

    HomeWork7 added another option to copy Message via Helper

--- hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -1,18 +1,25 @@
 package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
+import ru.otus.model.HelperMessage;
 import ru.otus.model.Message;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
+    Map<Long, Message> history = new HashMap<>();
+
 
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+//        history.put(msg.getId(), HelperMessage.getDeepCopy(msg));
+        history.put(msg.getId(), msg.copy());
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(history.get(id));
     }
 }

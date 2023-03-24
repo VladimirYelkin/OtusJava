b3750940e11b55d5c6609c4f2cefa0a539b2f1f8
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-03-24 11:53:12 +0400

    HomeWork7 add implement Copyable at ObjectForMessage

--- hw07-patterns/src/main/java/ru/otus/model/ObjectForMessage.java
+++ hw07-patterns/src/main/java/ru/otus/model/ObjectForMessage.java
@@ -1,8 +1,9 @@
 package ru.otus.model;
 
-import java.util.List;
+import java.util.*;
 
-public class ObjectForMessage {
+
+public class ObjectForMessage implements Copyable<ObjectForMessage> {
     private List<String> data;
 
     public List<String> getData() {
@@ -11,5 +12,17 @@ public class ObjectForMessage {
 
     public void setData(List<String> data) {
         this.data = data;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public ObjectForMessage copy() {
+        ObjectForMessage objectForCopy = new ObjectForMessage();
+        try {
+            objectForCopy.data = data.getClass().getConstructor(Collection.class).newInstance(data);
+        } catch (Exception e) {
+            objectForCopy.data = Optional.ofNullable(data).stream().flatMap(Collection::stream).toList();
+        }
+        return objectForCopy;
     }
 }

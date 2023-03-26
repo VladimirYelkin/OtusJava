9dc1ba9d45e0ee671406fc4a4ab9deaceaaf15ed
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-03-26 21:11:14 +0400

    HomeWork8 update ResourcesFileLoader.java

--- hw08-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,29 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import ru.otus.model.Measurement;
+
+import java.io.*;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
+    private final String fileName;
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        try (var jsonReader = new JsonReader(new BufferedReader(new FileReader(fileName)))) {
+            Gson gson = new Gson();
+            return gson.fromJson(jsonReader,new TypeToken<List<Measurement>>(){}.getType());
+        } catch (IOException e ) {
+            throw new FileProcessException(e);
+        }
+
     }
+
 }

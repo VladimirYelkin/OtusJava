914065781862cbe713ab6e7daf4523f2d9a15344
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-03-27 15:34:57 +0400

    HomeWork6 - fix in giveMoney in StorageCasesIml

--- hw06-solid/src/main/java/ru/otus/Demo.java
+++ hw06-solid/src/main/java/ru/otus/Demo.java
@@ -32,6 +32,8 @@ public class Demo {
         showGetFromAtmBanknotes(atm, 2000); // или 2000
         showPutInAtmBanknote(atm, BanknotesImpl.TWOTHOUSAND); // корзины для 2000 нет - должно быть сообщение об ошибке
         showPutInAtmBanknote(atm, BanknotesImpl.FIVETHOUSAND); // корзина для 5000 есть
+        showGetFromAtmBanknotes(atm, 6550); // или 2000
+        showGetFromAtmBanknotes(atm, 0); // или 2000
 
     }
 

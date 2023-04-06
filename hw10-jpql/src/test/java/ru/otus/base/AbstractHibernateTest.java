289314d770e2d81a4b981a3ee71f7d0988c55d57
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-04-06 15:15:20 +0400

    HomeWork10 jpql changed in AbstractHibernateTest: added Address and Phone class in builder session factory

--- hw10-jpql/src/test/java/ru/otus/base/AbstractHibernateTest.java
+++ hw10-jpql/src/test/java/ru/otus/base/AbstractHibernateTest.java
@@ -11,9 +11,13 @@ import ru.otus.core.repository.DataTemplateHibernate;
 import ru.otus.core.repository.HibernateUtils;
 import ru.otus.core.sessionmanager.TransactionManagerHibernate;
 import ru.otus.crm.dbmigrations.MigrationsExecutorFlyway;
+import ru.otus.crm.model.Address;
 import ru.otus.crm.model.Client;
+import ru.otus.crm.model.Phone;
 import ru.otus.crm.service.DBServiceClient;
 import ru.otus.crm.service.DbServiceClientImpl;
+
+import javax.swing.text.AbstractDocument;
 
 import static ru.otus.demo.DbServiceDemo.HIBERNATE_CFG_FILE;
 
@@ -51,7 +55,7 @@ public abstract class AbstractHibernateTest {
         configuration.setProperty("hibernate.connection.username", dbUserName);
         configuration.setProperty("hibernate.connection.password", dbPassword);
 
-        sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class);
+        sessionFactory = HibernateUtils.buildSessionFactory(configuration, Client.class, Address.class, Phone.class);
 
         transactionManager = new TransactionManagerHibernate(sessionFactory);
         clientTemplate = new DataTemplateHibernate<>(Client.class);

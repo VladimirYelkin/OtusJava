72c12da1bce0cb3ea7f51c367f39e2e51493f88f
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-04-16 18:55:15 +0400

    HomeWork11 cache engine added in DbServiceClientImpl.java work with cache

--- hw11-cacheEngine/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
+++ hw11-cacheEngine/src/main/java/ru/otus/crm/service/DbServiceClientImpl.java
@@ -2,22 +2,30 @@ package ru.otus.crm.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.otus.cache.HwCache;
 import ru.otus.core.repository.DataTemplate;
-import ru.otus.crm.model.Client;
 import ru.otus.core.sessionmanager.TransactionManager;
+import ru.otus.crm.model.Client;
 
 import java.util.List;
 import java.util.Optional;
 
+// run test from gradle
 public class DbServiceClientImpl implements DBServiceClient {
     private static final Logger log = LoggerFactory.getLogger(DbServiceClientImpl.class);
 
     private final DataTemplate<Client> clientDataTemplate;
     private final TransactionManager transactionManager;
+    private final HwCache<String, Client> cache;
 
     public DbServiceClientImpl(TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate) {
+        this(transactionManager, clientDataTemplate, null);
+    }
+
+    public DbServiceClientImpl(TransactionManager transactionManager, DataTemplate<Client> clientDataTemplate, HwCache<String, Client> kvMyCache) {
         this.transactionManager = transactionManager;
         this.clientDataTemplate = clientDataTemplate;
+        this.cache = kvMyCache;
     }
 
     @Override
@@ -27,19 +35,28 @@ public class DbServiceClientImpl implements DBServiceClient {
             if (client.getId() == null) {
                 clientDataTemplate.insert(session, clientCloned);
                 log.info("created client: {}", clientCloned);
+                saveClientToCache(clientCloned);
                 return clientCloned;
             }
             clientDataTemplate.update(session, clientCloned);
             log.info("updated client: {}", clientCloned);
+            saveClientToCache(clientCloned);
             return clientCloned;
         });
     }
 
+
     @Override
     public Optional<Client> getClient(long id) {
+        var clientFromCache = getClientFromCache(String.valueOf(id));
+        log.info("client in cache?: id={} value={}", id, clientFromCache);
+        if (clientFromCache.isPresent()) {
+            return clientFromCache;
+        }
         return transactionManager.doInReadOnlyTransaction(session -> {
             var clientOptional = clientDataTemplate.findById(session, id);
             log.info("client: {}", clientOptional);
+            clientOptional.ifPresent(this::saveClientToCache);
             return clientOptional;
         });
     }
@@ -50,6 +67,14 @@ public class DbServiceClientImpl implements DBServiceClient {
             var clientList = clientDataTemplate.findAll(session);
             log.info("clientList:{}", clientList);
             return clientList;
-       });
+        });
+    }
+
+    private Optional<Client> getClientFromCache(String id) {
+        return Optional.ofNullable(cache).map(cache1 -> cache1.get(id));
+    }
+
+    private void saveClientToCache(Client client) {
+        Optional.ofNullable(cache).ifPresent(cache1 -> cache1.put(String.valueOf(client.getId()), client));
     }
 }

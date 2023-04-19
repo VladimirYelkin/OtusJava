e7b825f136dfae8367cad4b131a1c9c96a8eba31
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-04-19 14:11:14 +0400

    HomeWork11 cache engine fix in Cache(move logger to List of Listiners);

--- hw11-cacheEngine/src/main/java/ru/otus/cache/MyCache.java
+++ hw11-cacheEngine/src/main/java/ru/otus/cache/MyCache.java
@@ -11,44 +11,42 @@ public class MyCache<K, V> implements HwCache<K, V> {
     private final Map<K, V> cacheMap = new WeakHashMap<>();
     private final List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
 
+    {   //logger.debug
+        listeners.add(new WeakReference<>((key, value, action) -> logger.debug("{}: logger  key:{}, value:{}", action, key, value)));
+    }
+
     //Надо реализовать эти методы
     @Override
     public void put(K key, V value) {
-        logger.debug("put: key:{} value{}  size WeekHashMap{}", key, value, cacheMap.size());
         event(key, value, "put");
         cacheMap.put(key, value);
     }
 
     @Override
     public void remove(K key) {
-        logger.debug("remove: key:{} size WeekHashMap{}", key, cacheMap.size());
         event(key, cacheMap.get(key), "remove");
         cacheMap.remove(key);
     }
 
     @Override
     public V get(K key) {
-        logger.debug("get: key:{} size WeekHashMap{}", key, cacheMap.size());
         event(key, cacheMap.get(key), "get");
         return cacheMap.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        logger.debug("addListener: {} ", listener);
         listeners.add(new WeakReference<>(listener));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        logger.debug("remove: {} ", listener);
         listeners.removeIf(hwListenerWeakReference ->
                 Optional.ofNullable(hwListenerWeakReference.get()).isEmpty() || listener.equals(hwListenerWeakReference.get())
         );
     }
 
     private void event(K key, V value, String action) {
-        logger.debug("event key:{} value:{} action:{} ", key, value, action);
 
         listeners.forEach(listener -> {
             try {

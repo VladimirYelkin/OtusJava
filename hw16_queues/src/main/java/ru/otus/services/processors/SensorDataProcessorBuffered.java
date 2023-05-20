8f45d72560124d40c7bba48119dbdb634b9dd4c5
Vladimir Yelkin <vl.yelkin@gmail.com>
2023-05-20 21:23:30 +0400

    HomeWork 16 queues solution in SensorDataProcessorBuffered (use PriorityBlockingQueue)

--- hw16_queues/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16_queues/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -6,30 +6,41 @@ import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 import ru.otus.lib.SensorDataBufferedWriter;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 // Этот класс нужно реализовать
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
-
+    private final BlockingQueue<SensorData> bufferedData;
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
+        this.bufferedData = new PriorityBlockingQueue<>(bufferSize + bufferSize >> 1, Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
     public void process(SensorData data) {
-    /*
-        if (dataBuffer.size() >= bufferSize) {
+        if (bufferedData.size() >= bufferSize) {
             flush();
         }
-    */
+        if (data != null) {
+            bufferedData.offer(data);
+        }
     }
 
     public void flush() {
         try {
-            //writer.writeBufferedData(bufferedData);
+            List<SensorData> buff = new ArrayList<>(bufferSize);
+            if (bufferedData.drainTo(buff, bufferSize) != 0) {
+                writer.writeBufferedData(buff);
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
